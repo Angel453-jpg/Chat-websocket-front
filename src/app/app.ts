9fa57e848a91f79b93647493b95ef85475e638c9
Angel <angelin09ozoz@gmail.com>
2025-10-31 17:50:03 -0600

    Primera configuracion del front

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import {Chat} from './components/chat/chat';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [
+    Chat
+  ],
+  templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('6-angular-chat');
 }
